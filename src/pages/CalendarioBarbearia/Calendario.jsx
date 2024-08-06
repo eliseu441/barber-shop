fb2b7f323ebb938c40a24812c8a80e8fde3270ca
eliseu441 <eliseu441@gmail.com>
2024-08-06 18:23:17 -0300

    teste calendario 2

--- src/pages/CalendarioBarbearia/Calendario.jsx
+++ src/pages/CalendarioBarbearia/Calendario.jsx
@@ -1,40 +1,48 @@
 import React, { useState } from 'react';
-import FullCalendar from '@fullcalendar/react';
-import dayGridPlugin from '@fullcalendar/daygrid';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+const localizer = momentLocalizer(moment);
 
 const Calendario = () => {
   const [events, setEvents] = useState([
     {
       title: 'Reunião com cliente',
-      start: '2023-10-10T10:00:00',
-      end: '2023-10-10T12:00:00',
+      start: new Date(2023, 9, 10, 10, 0), // 10 de Outubro de 2023, 10:00 AM
+      end: new Date(2023, 9, 10, 12, 0),   // 10 de Outubro de 2023, 12:00 PM
     },
     {
       title: 'Consulta médica',
-      start: '2023-10-11T14:00:00',
-      end: '2023-10-11T15:00:00',
+      start: new Date(2023, 9, 11, 14, 0), // 11 de Outubro de 2023, 2:00 PM
+      end: new Date(2023, 9, 11, 15, 0),   // 11 de Outubro de 2023, 3:00 PM
     },
     {
       title: 'Almoço com amigo',
-      start: '2023-10-12T12:00:00',
-      end: '2023-10-12T13:00:00',
+      start: new Date(2023, 9, 12, 12, 0), // 12 de Outubro de 2023, 12:00 PM
+      end: new Date(2023, 9, 12, 13, 0),   // 12 de Outubro de 2023, 1:00 PM
     },
   ]);
 
-  const handleDateClick = (arg) => {
+  const handleSelectSlot = ({ start, end }) => {
     const title = window.prompt('Novo evento:');
     if (title) {
-      setEvents([...events, { title, start: arg.date, end: arg.date }]);
+      setEvents([...events, { start, end, title }]);
     }
   };
 
   return (
     <div className="pagina-calendario col-sm-12">
-      <FullCalendar
-        plugins={[dayGridPlugin]}
-        initialView="dayGridMonth"
+      <Calendar
+        localizer={localizer}
         events={events}
-        dateClick={handleDateClick}
+        startAccessor="start"
+        endAccessor="end"
+        selectable
+        onSelectSlot={handleSelectSlot}
+        style={{ height: '80vh' }}
       />
     </div>
   );
