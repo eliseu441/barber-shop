81468ab6c6acac42057e47350c9b6c5032fce44a
eliseu441 <eliseu441@gmail.com>
2024-08-07 13:21:51 -0300

    teste portal

--- src/app.jsx
+++ src/app.jsx
@@ -1,23 +1,11 @@
-import { useState } from 'preact/hooks';
-import { Route, Routes, HashRouter } from 'react-router-dom';
-import Cadastro from './pages/cadastro/Cadastro';
-import Barber from './pages/Barbearia/homeBarber/Barber';
-import Calendario from './pages/CalendarioBarbearia/Calendario';
-import './scss/styles.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
+import { h } from 'preact';
 
-function App() {
+const App = () => {
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Cadastro />} />
-        <Route path="/barber" element={<Barber />} />
-        <Route path="/calendario" element={<Calendario />} />
-      </Routes>
-    </HashRouter>
+    <div className="container">
+      <h1>Hello, Preact!</h1>
+    </div>
   );
-}
+};
 
 export default App;
