373f3da9e0666806e31ae188f66ae8e2edec03bb
eliseu441 <eliseu441@gmail.com>
2024-08-07 15:00:50 -0300

    teste calendar

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import Cadastro from './pages/cadastro/Cadastro';
 import Barber from './pages/Barbearia/homeBarber/Barber';
+import Calendario from './pages/CalendarioBarbearia/Calendario';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Cadastro />} />
         <Route path="/barber" element={<Barber />} />
+        <Route path="/calendario" element={<Calendario />} />
       </Routes>
     </HashRouter>
   );
