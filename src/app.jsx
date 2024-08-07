52bc0deec8468748d9042f1a65daa4048169bc4d
eliseu441 <eliseu441@gmail.com>
2024-08-07 14:55:26 -0300

    remove calendar test

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,6 @@
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import Cadastro from './pages/cadastro/Cadastro';
 import Barber from './pages/Barbearia/homeBarber/Barber';
-import Calendario from './pages/CalendarioBarbearia/Calendario';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
