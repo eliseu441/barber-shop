84fa90b884ada7b408f52ec3face4f30953dd25d
eliseu441 <eliseu441@gmail.com>
2024-08-07 14:59:07 -0300

    teste page barber

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,6 @@
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import Cadastro from './pages/cadastro/Cadastro';
+import Barber from './pages/Barbearia/homeBarber/Barber';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
@@ -10,6 +11,7 @@ function App() {
     <HashRouter>
       <Routes>
         <Route path="/" element={<Cadastro />} />
+        <Route path="/barber" element={<Barber />} />
       </Routes>
     </HashRouter>
   );
