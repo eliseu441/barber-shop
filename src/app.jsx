c617b2ff0794552aa1fdae7b9ea2e9b11d7304fd
eliseu441 <eliseu441@gmail.com>
2024-08-07 14:11:19 -0300

    nao deu certo

--- src/app.jsx
+++ src/app.jsx
@@ -4,11 +4,8 @@ import Cadastro from './pages/cadastro/Cadastro';
 import Barber from './pages/Barbearia/homeBarber/Barber';
 import Calendario from './pages/CalendarioBarbearia/Calendario';
 import './scss/styles.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
 
-const App = () => {
+function App() {
   return (
     <div className="container">
       <h1>Hello, Preact!</h1>
