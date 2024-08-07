3456fc272fad5c58ff4df5edec8b678fbd976593
eliseu441 <eliseu441@gmail.com>
2024-08-07 14:19:34 -0300

    att cache

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,17 @@
+import { useState } from 'preact/hooks';
+import { Route, Routes, HashRouter } from 'react-router-dom';
+import Cadastro from './pages/cadastro/Cadastro';
+import Barber from './pages/Barbearia/homeBarber/Barber';
+import Calendario from './pages/CalendarioBarbearia/Calendario';
+import './scss/styles.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
-
-const App = () => {
+function App() {
   return (
     <div className="container">
-      <h1>Hello, Preacta!</h1>
+      <h1>Hello, Preact!</h1>
     </div>
   );
 }
