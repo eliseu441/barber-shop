84ad777b86463160658b4577f545e2a1333ad1aa
eliseu441 <eliseu441@gmail.com>
2024-08-07 09:30:43 -0300

    att dist

--- monolit/dist/src/app.module.js
+++ monolit/dist/src/app.module.js
@@ -23,7 +23,7 @@ exports.AppModule = AppModule = __decorate([
     (0, common_1.Module)({
         imports: [
             serve_static_1.ServeStaticModule.forRoot({
-                rootPath: (0, path_1.join)(__dirname, '..', 'portal'),
+                rootPath: (0, path_1.join)(__dirname, '../..', 'portal'),
             }),
             typeorm_1.TypeOrmModule.forRoot({
                 type: 'postgres',
