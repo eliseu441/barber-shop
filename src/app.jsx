7b41be49ac028483e956dc5b707eaa404e30b912
eliseu441 <eliseu441@gmail.com>
2024-08-07 14:16:21 -0300

    function app

--- src/app.jsx
+++ src/app.jsx
@@ -3,7 +3,8 @@ import Cadastro from './pages/cadastro/Cadastro';
 import Barber from './pages/Barbearia/homeBarber/Barber';
 import Calendario from './pages/CalendarioBarbearia/Calendario';
 
-function App() {
+
+const App = () => {
   return (
     <div className="container">
       <h1>Hello, Preacta!</h1>
