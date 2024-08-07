6a2ab600f893c5c8c62e2a4a2aab5ba2c221897a
eliseu441 <eliseu441@gmail.com>
2024-08-07 13:24:17 -0300

    inferno de cache

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,23 @@
-import { h } from 'preact';
+import { useState } from 'preact/hooks';
+import { Route, Routes, HashRouter } from 'react-router-dom';
+import Cadastro from './pages/cadastro/Cadastro';
+import Barber from './pages/Barbearia/homeBarber/Barber';
+import Calendario from './pages/CalendarioBarbearia/Calendario';
+import './scss/styles.scss';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
 
-const App = () => {
+function App() {
   return (
-    <div className="container">
-      <h1>Hello, Preact!</h1>
-    </div>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Cadastro />} />
+        <Route path="/barber" element={<Barber />} />
+        <Route path="/calendario" element={<Calendario />} />
+      </Routes>
+    </HashRouter>
   );
-};
+}
 
 export default App;
