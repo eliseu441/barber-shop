97f99f95640f069a08bdf299583be9dd5488d952
eliseu441 <eliseu441@gmail.com>
2024-08-07 13:55:26 -0300

    teste 4

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,10 @@
-import { useState } from 'preact/hooks';
-import { Route, Routes, HashRouter } from 'react-router-dom';
-import Cadastro from './pages/cadastro/Cadastro';
-import Barber from './pages/Barbearia/homeBarber/Barber';
-import Calendario from './pages/CalendarioBarbearia/Calendario';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
+
 
 function App() {
   return (
-    <div>teste</div>
+    <div className="container">
+      <h1>Hello, Preact!</h1>
+    </div>
   );
 }
 
