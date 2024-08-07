264a99d1d88db00b119caf7de0b71e1a24ef7a92
eliseu441 <eliseu441@gmail.com>
2024-08-07 14:06:21 -0300

    no scss

--- src/app.jsx
+++ src/app.jsx
@@ -3,11 +3,6 @@ import { Route, Routes, HashRouter } from 'react-router-dom';
 import Cadastro from './pages/cadastro/Cadastro';
 import Barber from './pages/Barbearia/homeBarber/Barber';
 import Calendario from './pages/CalendarioBarbearia/Calendario';
-import './scss/styles.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import 'react-big-calendar/lib/css/react-big-calendar.css';
-
 const App = () => {
   return (
       <Routes>
