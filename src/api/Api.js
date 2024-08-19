fae251340e548caaf1bee0276455ed7f6e73681e
eliseu441 <eliseu441@gmail.com>
2024-08-19 03:39:07 -0300

    ...

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 //console.log(import.meta.env.VITE_URL_API)
-const baseURL = `http://localhost:8080/api/`
-//const baseURL = `/api`
+//const baseURL = `http://localhost:8080/api/`
+const baseURL = `/api`
 //const baseURL = import.meta.env.VITE_URL_API
 
 export default axios.create({
