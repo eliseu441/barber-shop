f424380672053b344c8c2d4f403c16acb8ab7071
eliseu441 <eliseu441@gmail.com>
2024-08-19 02:08:17 -0300

    teste vite changes

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,10 @@ import preact from '@preact/preset-vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/monolit/portal/', // Adjust this if your assets are served from a different path
   plugins: [preact()],
   build: {
+    outDir: './monolit/portal', // Custom output directory
     rollupOptions: {
       output: {
         format: 'es', // Use ES modules
