1c5c1ded1719e0b4e30eda90a231cf1fe1d92429
eliseu441 <eliseu441@gmail.com>
2024-08-19 09:35:09 -0300

    teste api

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,8 +1,5 @@
 import axios from 'axios';
-//console.log(import.meta.env.VITE_URL_API)
-//const baseURL = `http://localhost:8080/api/`
 const baseURL = `/api`
-//const baseURL = import.meta.env.VITE_URL_API
 
 export default axios.create({
 	baseURL,
