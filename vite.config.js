7dcd6e66dbbfa712c843d6cb1dbaa2b28fa5a62e
eliseu441 <eliseu441@gmail.com>
2024-08-19 03:02:51 -0300

    att build

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-import { defineConfig } from 'vite';
-import preact from '@preact/preset-vite';
+import { defineConfig } from 'vite'
+import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
   plugins: [preact()],
-  build: {
-    outDir: './monolit/portal', // Certifique-se de que esse diretório existe e está correto
-    rollupOptions: {
-      output: {
-        format: 'es', // Use ES modules
-      },
-    },
-  },
-});
+})
